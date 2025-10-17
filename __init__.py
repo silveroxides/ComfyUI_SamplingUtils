e021a5dd6074591ae77428147b3c0954470cf7ea
silveroxides <ishimarukaito@gmail.com>
2025-10-17 08:30:06 +0200

    Add SamplingParameters node

---
+++ __init__.py
@@ -0,0 +1,45 @@
+from typing_extensions import override
+from comfy_api.latest import ComfyExtension, io
+import nodes
+
+def round_to_nearest(n, m):
+    return int((n + (m / 2)) // m) * m
+
+class SamplingParameters(io.ComfyNode):
+    @classmethod
+    def define_schema(cls) -> io.Schema:
+        return io.Schema(
+            node_id="SamplingParameters",
+            category="utils",
+            inputs=[
+                io.Int.Input(id="width", default=1024, min=16, max=nodes.MAX_RESOLUTION, step=16),
+                io.Int.Input(id="height", default=1024, min=16, max=nodes.MAX_RESOLUTION, step=16),
+                io.Int.Input(id="batch_size", default=1, min=1, max=4096),
+                io.Float.Input(id="scale_by", default=1.0, min=0.0, max=10.0, step=0.01, tooltip="How much to upscale initial resolution by for the upscaled one."),
+                io.Int.Input(id="multiple", default=16, min=4, max=128, step=4, tooltip="Nearest multiple of the result to set the upscaled resolution to."),
+            ],
+            outputs=[
+                io.Int.Output(display_name="width"),
+                io.Int.Output(display_name="height"),
+                io.Int.Output(display_name="batch_size"),
+                io.Int.Output(display_name="upscaled_width"),
+                io.Int.Output(display_name="upscaled_height"),
+            ],
+        )
+
+    @classmethod
+    def execute(cls, *, width: int, height: int, batch_size: int=1, scale_by: float, multiple: int) -> io.NodeOutput:
+        upscaled_width = round_to_nearest(int(width*scale_by), int(multiple))
+        upscaled_height = round_to_nearest(int(height*scale_by), int(multiple))
+        return io.NodeOutput(width, height, batch_size, upscaled_width, upscaled_height)
+
+class SamplingUtils(ComfyExtension):
+    @override
+    async def get_node_list(self) -> list[type[io.ComfyNode]]:
+        return [
+            SamplingParameters,
+        ]
+
+
+async def comfy_entrypoint() -> SamplingUtils:
+    return SamplingUtils()
